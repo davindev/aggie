a9a5ee78610159ad7bc257d989c32ed04c6407f5
davindev <vinnie.jeong@kidsnote.com>
2023-03-22 14:26:55 +0900

    chore: eslint 설정 추가

---
+++ simple-aggie-server/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 'off',
+  },
+};
--- simple-aggie-server/src/index.ts
+++ simple-aggie-server/src/index.ts
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+
+const app = express();
+const port = 3000;
 
 app.get('/', (req: any, res: any) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
