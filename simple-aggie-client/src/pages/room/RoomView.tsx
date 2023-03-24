c881b28b3e70717e98f261f57fdee21f63e8621b
davindev <vinnie.jeong@kidsnote.com>
2023-03-24 13:10:40 +0900

    feat: 소켓 연결 및 웰컴 메세지 수신 기능 추가

--- simple-aggie-client/src/pages/room/RoomView.tsx
+++ simple-aggie-client/src/pages/room/RoomView.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { io } from 'socket.io-client';
 
 export default function RoomView() {
+  const { roomId } = useParams();
+
+  const socket = io('http://localhost:8080');
+
+  useEffect(() => {
+    socket.connect();
+
+    const username = `Anonymous#${Math.random().toString(36).substring(2)}`;
+    socket.emit('join-room', { roomId, username });
+
+    socket.on('welcome-message', (message: string) => console.log(message));
+  }, [socket, roomId]);
+
   return (
-    <div>RoomView</div>
+    <div>
+      Room
+    </div>
   );
 }
--- simple-aggie-server/src/index.ts
+++ simple-aggie-server/src/index.ts
@@ -1,12 +1,24 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
-const port = 3000;
+const server = http.createServer(app).listen(8080);
+const io = new Server(
+  server,
+  { cors: { origin: 'http://localhost:3001' } },
+);
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello World!');
+interface JoinRoom {
+  roomId: string;
+  username: string;
+}
+
+io.on('connection', (socket: any) => {
+  socket.on('join-room', ({ roomId, username }: JoinRoom) => {
+    socket.join(roomId);
+    socket.emit('welcome-message', `${username}님이 입장하셨습니다.`);
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export {};
