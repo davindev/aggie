d921c0a9698b2b7f322efd7493b67ce7478fcc91
davindev <vinnie.jeong@kidsnote.com>
2023-03-28 11:23:57 +0900

    feat: 마우스 위치 정보 표시 기능 추가

--- simple-aggie-client/src/pages/room/RoomView.tsx
+++ simple-aggie-client/src/pages/room/RoomView.tsx
@@ -1,30 +1,75 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, {
+  useRef,
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
 import { useParams } from 'react-router-dom';
 import { io } from 'socket.io-client';
 
 import getRandomString from '../../utils/getRandomString';
 
+interface MousePosition {
+  x: number;
+  y: number;
+  color: string;
+}
+
 export default function RoomView() {
   const { roomId } = useParams();
+
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const ctx = canvasRef.current?.getContext('2d');
+
   const [isPainting, setIsPainting] = useState(false);
+
   const socket = io('http://localhost:8080');
 
-  const handlePaintCanvas = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
-    const x = event.nativeEvent.offsetX;
-    const y = event.nativeEvent.offsetY;
-    const ctx = canvasRef.current?.getContext('2d');
+  const handlePaintOnCanvas = useCallback(({ x, y, color }: MousePosition) => {
+    if (ctx) {
+      ctx.strokeStyle = color;
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x, y);
+      ctx.stroke();
+      ctx.closePath();
+      socket.emit('paint-on-canvas', { x, y, color });
+    }
+  }, [ctx, socket]);
 
-    // socket.emit('painting-canvas', { x, y });
+  const handleMouseMoveOnCanvas = useCallback(({ x, y, color }: MousePosition) => {
+    if (ctx) {
+      const canvas = canvasRef.current as HTMLCanvasElement;
+      const rect = canvas.getBoundingClientRect();
+      const rectX = x - rect.left;
+      const rectY = y - rect.top;
 
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(rectX, rectY, 8, 0, 2 * Math.PI);
+      ctx.fillStyle = color;
+      ctx.fill();
+    }
+  }, [ctx]);
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLCanvasElement, MouseEvent>) => {
     if (isPainting) {
-      ctx?.lineTo(x, y);
-      ctx?.stroke();
+      const x = event.nativeEvent.offsetX;
+      const y = event.nativeEvent.offsetY;
+      handlePaintOnCanvas({ x, y, color: 'green' });
       return;
     }
 
-    ctx?.beginPath();
-    ctx?.moveTo(x, y);
+    const mousePosition = {
+      x: event.clientX,
+      y: event.clientY,
+      color: 'green',
+    };
+
+    handleMouseMoveOnCanvas(mousePosition);
+
+    // 마우스 위치 정보 발신
+    socket.emit('mouse-move-on-canvas', mousePosition);
   };
 
   useEffect(() => {
@@ -32,12 +77,22 @@ export default function RoomView() {
     socket.connect();
 
     // 방 입장
-    const username = `Anonymous#${getRandomString()}`;
+    const username = `anonymous#${getRandomString()}`;
     socket.emit('join-room', { roomId, username });
 
     // 웰컴 메세지 노출
-    socket.on('welcome-message', (message: string) => console.log(message));
+    socket.on('send-welcome-message', (message: string) => console.log(message));
   }, [socket, roomId]);
+
+  useEffect(() => {
+    // 마우스 위치 정보 수신
+    socket.on('mouse-move-on-canvas', handleMouseMoveOnCanvas);
+  }, [socket, handleMouseMoveOnCanvas]);
+
+  // useEffect(() => {
+  //   // 캔버스 path 데이터 수신
+  //   socket.on('paint-on-canvas', handlePaintOnCanvas);
+  // }, [socket, handlePaintOnCanvas]);
 
   return (
     <div>
@@ -46,7 +101,7 @@ export default function RoomView() {
         width={800}
         height={800}
         onMouseDown={() => setIsPainting(true)}
-        onMouseMove={handlePaintCanvas}
+        onMouseMove={handleMouseMove}
         onMouseUp={() => setIsPainting(false)}
         onMouseLeave={() => setIsPainting(false)}
       />
--- simple-aggie-server/src/index.ts
+++ simple-aggie-server/src/index.ts
@@ -14,10 +14,22 @@ interface JoinRoom {
   username: string;
 }
 
+interface MousePosition {
+  x: number;
+  y: number;
+  color: string;
+}
+
 io.on('connection', (socket: any) => {
+  // 방 입장
   socket.on('join-room', ({ roomId, username }: JoinRoom) => {
     socket.join(roomId);
-    socket.emit('welcome-message', `${username}님이 입장하셨습니다.`);
+    socket.emit('send-welcome-message', `${username}님이 입장하셨습니다.`);
+  });
+
+  // 마우스 위치 정보 전달
+  socket.on('mouse-move-on-canvas', (mousePosition: MousePosition) => {
+    socket.broadcast.emit('mouse-move-on-canvas', mousePosition);
   });
 });
 
